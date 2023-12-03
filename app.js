4ac000a04c9162ab1a081fb248447d0690a21eee
SravanaDeepthi <152780652+SravanaDeepthi@users.noreply.github.com>
2023-12-03 16:34:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+// $(window).on('load', function(){
+//     $('.Preloader-child').fadeOut();
+//     $('#preloader').delay(350).fadeOut();
+//   });
+
+const input = document.getElementById('input');
+const list = document.getElementById('ul');
+
+function addTask(){
+    if(input.value === ''){
+        alert("you must enter some text");
+    }
+    else{
+    
+        let li = document.createElement('li');
+        let btn1=document.createElement('button');
+        let btn2=document.createElement('button');
+        li.innerHTML = input.value;
+        btn1.innerHTML="Done";
+        btn2.innerHTML="Delete";
+        list.appendChild(li);
+        li.appendChild(btn1);
+        li.appendChild(btn2);
+        // Adding Styles to buttons
+        btn1.style.right="100px";
+
+        // Adding button functionality
+        btn1.addEventListener('click',
+        function(){
+            li.style.textDecoration="line-through";
+        }
+        );
+        btn2.addEventListener('click',
+        function(){
+            li.remove();
+        }
+        );
+    }input.value = "";
+
+}
